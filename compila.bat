db49bb7f075ac18aaadfd07fa58bab21009ea58c
Sen <88703078+senwawa@users.noreply.github.com>
2025-01-27 16:44:51 +0100

    Updated compila.bat
    changed AVIUTL_IT_PATH to RES_PATH

--- compila.bat
+++ compila.bat
@@ -3,15 +3,15 @@ TITLE compilazione aviutl-italian-patch
 
 :: Definizione delle variabili per le path
 SET MINGW_PATH="Rimpiazzami"
-SET AVIUTL_IT_PATH="Rimpiazzami"
+SET RES_PATH="Rimpiazzami"
 SET AVIUTL_EN_PATH="Rimpiazzami"
 
 :: Aggiungi la path di MinGW al PATH di sistema
 SET PATH=%MINGW_PATH%;%PATH%
 
 :: Compilazione delle risorse
-windres.exe -v --input-format=rc --output-format=coff "%AVIUTL_IT_PATH%\it_resource.rc" -o "%AVIUTL_IT_PATH%\it_resource.res"
-gcc -Os -m32 -s -o "%AVIUTL_IT_PATH%\it_mod.aul" -shared -Wl,-s "%AVIUTL_IT_PATH%\it_resource.res"
+windres.exe -v --input-format=rc --output-format=coff "%RES_PATH%\it_resource.rc" -o "%RES_PATH%\it_resource.res"
+gcc -Os -m32 -s -o "%RES_PATH%\it_mod.aul" -shared -Wl,-s "%RES_PATH%\it_resource.res"
 
 :: Messaggio di completamento compilazione
 echo Compilazione finita!
@@ -20,7 +20,7 @@ echo Compilazione finita!
 del "%AVIUTL_EN_PATH%\it_mod.aul"
 
 :: Copia il file compilato nella directory corretta
-copy "%AVIUTL_IT_PATH%\it_mod.aul" "%AVIUTL_EN_PATH%\"
+copy "%RES_PATH%\it_mod.aul" "%AVIUTL_EN_PATH%\"
 
 :: Messaggio di sostituzione
 echo File sostituito!
